60a18a0c3ca764ebdb5a53b56df8006e844c4e3b
ouyangzling1990 <wujingwei1990@126.com>
2018-12-14 14:35:03 +0800

    定制页面展示时echart折线图的展示标准

--- static/js/zilingUtil.js
+++ static/js/zilingUtil.js
@@ -167,3 +167,24 @@ function getScrollOffsets(w) {
     }
     return {x: d.body.scrollLeft, y:d.body.scrollTop}
 }
+
+function throttle(fn,interval){
+    var last;
+    var timer;
+    var interval=interval||200;
+    return function(){
+        var th=this;
+        var args=arguments;
+        var now=+new Date();
+        if(last&&now-last<interval){
+            clearTimeout(timer);
+            timer=setTimeout(function(){
+                last=now;
+                fn.apply(th,args);
+            },interval);
+        }else{
+            last=now;
+            fn.apply(th,args);
+        }
+    }
+}
