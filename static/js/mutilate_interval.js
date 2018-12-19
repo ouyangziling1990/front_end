ad0efa4404cd76d40564be00b0159dcfcf7077e1
ouyangzling1990 <wujingwei1990@126.com>
2018-12-19 20:44:08 +0800

    add clearInterval block

--- static/js/mutilate_interval.js
+++ static/js/mutilate_interval.js
@@ -2,13 +2,23 @@ jQuery(function($) {
     function con(i) {
         console.log(i)
     }
+    var intervals = []
     for (var i = 0; i < 4; i++) {
-        createInterval(con, i, 1000)
+        var tmp = createInterval(con, i, 1000)
+        intervals.push(tmp)
     }
 
+    setTimeout(function(){
+        intervals.forEach(function(e){
+            clearInterval(e)
+        })
+    }, 3*1000)
+
+
     function createInterval(f, dynamicParameter, interval) {
-        setInterval(function() { 
+        var int = setInterval(function() { 
             f(dynamicParameter);
         }, interval);
+        return int;
     }
 })
